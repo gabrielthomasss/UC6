41857534a086f572abac5e16944652715f3a8d6f
GabrielTSL <159187607+gabrielthomasss@users.noreply.github.com>
2024-03-15 16:42:25 -0300

    Att

---
+++ SQLQuery2.sql
@@ -0,0 +1,55 @@
+use sistema_biblioteca;
+
+--Questão 1
+select count(*) as total_livros
+from LIVRO_BIBLIOTECA
+where cnpj= '12345678000123';
+--Questão 2
+select * from BIBLIOTECARIO order by nome;
+
+--Questão 3
+select LIVRO
+
+--Questão 4
+select *
+from evento
+where tipo = 'workshop' and custo > 150.00;
+
+--Questão 5 não
+select * from TECNOLOGIA;
+
+--Questão 6 sim
+select 
+
+
+--Questão 7 sim
+select 
+
+--Questão 8
+select numero_registro from SOCIOLOGIA;
+from 
+
+--11 não
+select *
+from evento
+where tipo = 'PALESTRA' and data <'2022'
+
+--12 não
+
+--Questão 13
+select TECNICO_TI.nome
+from TECNICO_TI
+where nome like '%rodrigo%';
+--Questão 14
+select * 
+from FUNCIONARIO
+where cpf = 
+--Questão 15
+select LIVRO
+
+--Questão 16
+
+
+--Questão 17
+
+--18 não
